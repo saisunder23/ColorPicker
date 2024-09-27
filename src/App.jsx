5a631dfd0012b56e9fd60c8947083eacb2090f15
Sunder Sai <saisunder23@gmail.com>
2024-09-26 23:02:06 -0400

    updates to color schema

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,35 @@
+import { useState } from 'react';
+
 // Write your Color component here
+const Color = ({ color, setSelectedColor }) => {
+  return (
+    <div 
+      className={color} 
+      onClick={() => setSelectedColor(color)} // Add onClick handler to set selected color
+    >
+      {color}
+    </div>
+  );
+};
 
 const App = () => {
+  // Invoke useState to manage selectedColor
+  const [selectedColor, setSelectedColor] = useState("red");
+
   return (
     <div id="container">
       <div id="navbar">
         <div>Currently selected: </div>
-        <div className="red">red</div>
+        {/* Display the selected color from state */}
+        <div className={selectedColor}>{selectedColor}</div>
       </div>
-      <div id="colors-list">{/* colors go here */}</div>
+
+      {/* Pass setSelectedColor down to each Color component */}
+      <div id="colors-list">
+        <Color color="red" setSelectedColor={setSelectedColor} />
+        <Color color="green" setSelectedColor={setSelectedColor} />
+        <Color color="blue" setSelectedColor={setSelectedColor} />
+      </div>
     </div>
   );
 };
